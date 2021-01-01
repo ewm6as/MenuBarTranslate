abfc0a3a19161203957d3e73740be55c72de4129
ewm6as <76823993+ewm6as@users.noreply.github.com>
2020-12-31 20:27:35 -0500

    Update menubartranslate.py

--- menubartranslate.py
+++ menubartranslate.py
@@ -6,7 +6,9 @@ To do:
 * Revise languages to include a list of all MESA/Romance languages
 """
 translator = google_translator()
-class Translate_App(object):
+
+
+class TranslateApp(object):
     def __init__(self):
         self.config = {
             "app_name": "Translate",
@@ -24,37 +26,37 @@ class Translate_App(object):
         self.clipboard_timer = rumps.Timer(self.on_tick, 2)
         self.app.menu = [self.translate_menu_button, self.clipboard_on_menu_button, self.clipboard_off_menu_button]
         self.old_clipboard = ''
+
     def run(self):
         self.app.run()
+
     def clipboard_on_callback(self, sender):
         self.clipboard_timer.start()
+
     def clipboard_off_callback(self, sender):
         self.clipboard_timer.stop()
         self.app.title = "ب"
-
 
     def on_tick(self, sender):
         current_clipboard = str(clipboard.paste())
         if current_clipboard != self.old_clipboard:
             self.app.title = self.translate_text(self, text=current_clipboard)
             self.old_clipboard = current_clipboard
+
     def translate_text(self, sender, text):
         # this one is slightly different for menu bar so it doesn't have the \n, maybe find a way to consolidate this
         input_source_lang = translator.detect(text)
-        if (input_source_lang[0]) == "fa" or input_source_lang[0] == "ar": #change to if it's in the list of specified languages (and have a list of MESA languages
-            #print("it's doing the if statement")
+        if (input_source_lang[0]) == "fa" or input_source_lang[0] == "ar": #change to list of MESA langs
             translation_en = translator.translate(text, lang_tgt="en", lang_src="ar")
             translation_es = translator.translate(text, lang_tgt="es", lang_src="ar")
             if type(translation_es) == list:
                 translation_es = translation_es[0]
             translation_message = translation_en + " " + translation_es
         elif (input_source_lang[0]) == "es" or input_source_lang[0] == "fr":
-            #print("it's doing the elif statement")
             translation_en = translator.translate(text, lang_tgt="en", lang_src="es")
             translation_ar = translator.translate(text, lang_tgt="ar", lang_src="es")
             translation_message = translation_en + " " + translation_ar
         else:
-            #print("it's doing the else statement")
             translation_es = translator.translate(text, lang_tgt="es", lang_src="en")
             if type(translation_es) == list:
                 translation_es = translation_es[0]
@@ -62,36 +64,30 @@ class Translate_App(object):
             translation_message = translation_es + " " + translation_ar
         return translation_message
 
-
-    def text_callback(self, sender): #for whatever reason, I need sender in the function for the function to callback
+    def text_callback(self, sender):
         self.request = str(self.text_window.run().text) #the run function returns the text and the button pressed
         input_source_lang = translator.detect(self.request)
-        #print(input_source_lang[0]) #to test what language it's detecting it as
-        if (input_source_lang[0]) == "fa" or input_source_lang[0] == "ar": #change to if it's in the list of specified languages (and have a list of MESA languages
-            #print("it's doing the if statement")
+        if (input_source_lang[0]) == "fa" or input_source_lang[0] == "ar": #change to list of MESA langs
             translation_en = translator.translate(self.request, lang_tgt="en", lang_src="ar")
             translation_es = translator.translate(self.request, lang_tgt="es", lang_src="ar")
             if type(translation_es) == list:
                 translation_es = translation_es[0]
             translation_message = self.request + "\n" + translation_en + "\n" + translation_es
         elif (input_source_lang[0]) == "es" or input_source_lang[0] == "fr":
-            #print("it's doing the elif statement")
             translation_en = translator.translate(self.request, lang_tgt="en", lang_src="es")
             translation_ar = translator.translate(self.request, lang_tgt="ar", lang_src="es")
             translation_message = self.request + "\n" + translation_en + "\n" + translation_ar
         else:
-            #print("it's doing the else statement")
             translation_es = translator.translate(self.request, lang_tgt="es", lang_src="en")
             if type(translation_es) == list: #a fix for Spanish words being returned as a list if gendered.
                 translation_es = translation_es[0]
             translation_ar = translator.translate(self.request, lang_tgt="ar", lang_src="en")
             translation_message = self.request + "\n" + translation_es + "\n" + translation_ar
-            #print(translation_message)
         self.translation_alert = rumps.alert(str(translation_message))
         self.translation_alert()
-        #self.translation_notification = rumps.notification(title="Translation", message=self.request)
-        #self.translation_notification() this did not work (did not send notif)
         return self.request
+
+
 if __name__ == '__main__':
-    app = Translate_App()
+    app = TranslateApp()
     app.run()
